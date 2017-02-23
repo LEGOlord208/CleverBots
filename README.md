3f79679af8b64e5220b6f9ee19b3741e85de86ee
LEGOlord208 <LEGOlord208@gmail.com>
2017-02-23 19:31:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CleverTalk
+Ever looked into making Cleverbot talk to... Cleverbot?  
+Well look no further **¯\\\_(ツ)_/¯**  
+
+Binaries at [My website](https://krake.one/software#clevertalk)
---
+++ main.go
@@ -0,0 +1,90 @@
+package main;
+
+import (
+	"fmt"
+	"github.com/legolord208/stdutil"
+	"os"
+	"os/signal"
+	"time"
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+)
+
+const URL = "https://www.cleverbot.com/getreply";
+
+var conv1 string;
+var conv2 string;
+var stopped bool;
+
+var turn bool;
+var reply string;
+
+func main(){
+	fmt.Print("Cleverbot key: ");
+	KEY := stdutil.MustScanTrim();
+
+	go func(){
+		c := make(chan os.Signal, 1);
+		signal.Notify(c, os.Interrupt);
+
+		<-c;
+		stopped = true;
+	}();
+
+	for i := 0; i < 3; i++ {
+		fmt.Println();
+	}
+
+	for{
+		if(stopped){ break; }
+		time.Sleep(3 * time.Second);
+		turn = !turn;
+		if(stopped){ break; }
+
+		turnStr := "1";
+		if(!turn){
+			turnStr = "2";
+		}
+		fmt.Print("CleverBot " + turnStr + ": ");
+
+		v := url.Values{};
+		v.Set("key", KEY);
+		v.Set("input", reply);
+
+		if(turn){
+			v.Set("cs", conv1);
+		} else {
+			v.Set("cs", conv2);
+		}
+
+		res, err := http.Get(URL + "?" + v.Encode());
+		if(err != nil){
+			stdutil.PrintErr("Could not make request", err);
+			break;
+		}
+		body, err := ioutil.ReadAll(res.Body);
+		res.Body.Close();
+		if(err != nil){
+			stdutil.PrintErr("Could not get data from request", err);
+			break;
+		}
+
+		replyMap := make(map[string]string);
+		err = json.Unmarshal(body, &replyMap);
+		if(err != nil){
+			stdutil.PrintErr("Could not parse JSON", err);
+			break;
+		}
+
+		if(turn){
+			conv1 = replyMap["cs"];
+		} else {
+			conv2 = replyMap["cs"];
+		}
+
+		reply = replyMap["output"];
+		fmt.Println(reply);
+	}
+}
